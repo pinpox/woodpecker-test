c3f1dd8f4d16bb3f4c2ae82fa451c043576538f7
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-08-11 15:45:23 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -29,11 +29,19 @@
             backend: local
           
           pipeline:
-            - name: Test commands
-              image: bash
-              commands:
-                - echo "HELLO WORLD2"
-            
+          - name: Test commands
+            image: bash
+            commands:
+              - echo "HELLO WORLD"
+              - nix --version
+              - nix doctor
+              - nix store ping  
+              - whereis nix
+              - which nix
+              - nix run 'nixpkgs#hello'
+              - nix flake show
+              - nix run
+     
         '';
       };
 
